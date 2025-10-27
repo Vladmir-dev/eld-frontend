7b4eae057066a9a6b42bab792a3c846621f5c261
Vladmir-dev <tempestpius70@gmail.com>
2025-10-27 18:47:59 +0300

    updated logs reducer import

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 // src/app/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import tripReducer from "../features/trips/tripSlice";
-import logReducer from "../features/logs/logSlice";
+import logReducer from "../features/logs/logSlice.js";
 import authReducer from "../features/auth/authSlice";
 
 const store = configureStore({

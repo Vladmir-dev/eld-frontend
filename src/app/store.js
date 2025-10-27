0c318ccef74f36b817edd81eb5643bf786e9d853
Vladmir-dev <tempestpius70@gmail.com>
2025-10-27 18:53:35 +0300

    updated logs reducer import with alias

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-// src/app/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import tripReducer from "../features/trips/tripSlice";
-import logReducer from "../features/logs/logSlice.js";
-import authReducer from "../features/auth/authSlice";
+// 1. Use alias for all imports
+import tripReducer from "@/features/trips/tripSlice.js";
+import logReducer from "@/features/logs/logSlice.js";
+import authReducer from "@/features/auth/authSlice.js";
 
 const store = configureStore({
   reducer: {
